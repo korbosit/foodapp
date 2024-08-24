fc2c0dba6659b9fcf68d550d712da10cd2fed189
Vlaimir Korba <krissky1986@gmail.com>
2024-08-24 15:52:25 +0300

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function Search() {
+    const [query, setQuery] = useState("");
+    // Syntax of the useEffect hook
+    useEffect(() => {
+        function demo() {
+            console.log("Demo function executed");
+        }
+        demo();
+    }, [query]);
+    return (
+        <div>
+            <input
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                type="text"
+            />
+        </div>
+    );
+}
