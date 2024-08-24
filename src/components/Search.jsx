cfcce6a92973ccf72b38c80026d058f8239de20f
Vlaimir Korba <krissky1986@gmail.com>
2024-08-24 17:00:41 +0300

    #50: Creating State To Save Food Data

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "15c6dc89ba1d4af49def1edb6b699117";
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
     const [query, setQuery] = useState("");
     // Syntax of the useEffect hook in react
     useEffect(() => {
@@ -11,6 +11,7 @@ export default function Search() {
             const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
             const data = await res.json();
             console.log(data.results);
+            setFoodData(data.results);
         }
         fetchFood();
     }, [query]);
