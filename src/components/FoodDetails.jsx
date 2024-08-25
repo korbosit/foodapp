d4a709dc4f78d306cd64b7594e394b4d530fb754
Vlaimir Korba <krissky1986@gmail.com>
2024-08-25 11:47:14 +0300

    #59_Fetching_Recipes_From_API

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from "react";
+
 export default function FoodDetails({ foodId }) {
-    return <div>Food Details {foodId}</div>;
+    const [food, setFood] = useState({});
+    // console.log("Received foodId:", foodId); // Выводим значение foodId
+    const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+    const API_KEY = "15c6dc89ba1d4af49def1edb6b699117";
+    useEffect(() => {
+        async function fetchFood() {
+            // console.log("Fetching data for foodId:", foodId); // Проверка перед запросом
+            const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+            const data = await res.json();
+            console.log(data);
+            setFood(data);
+        }
+        fetchFood();
+    }, [foodId]);
+    return (
+        <div>
+            Food Details {foodId}
+            {food.title}
+            <img src={food.image} alt="" />
+        </div>
+    );
 }
